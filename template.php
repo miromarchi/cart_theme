678b4b01f223d4ad36584c2d8c8958a28a9234dd
miromarchi <miro.marchi@gmail.com>
2014-10-10 21:23:20 +0200

    custom date separator here

--- template.php
+++ template.php
@@ -56,5 +56,32 @@ function cart_theme_form_alter(&$form, &$form_state, $form_id) {
   }
 }
 
+/**
+ * Overriding of date.theme _display_range function in order to set custom separator
+ * Returns HTML for a date element formatted as a range.
+ */
+function cart_theme_date_display_range($variables) {
+  $date1 = $variables['date1'];
+  $date2 = $variables['date2'];
+  $timezone = $variables['timezone'];
+  $attributes_start = $variables['attributes_start'];
+  $attributes_end = $variables['attributes_end'];
+
+  $start_date = '<span class="date-display-start"' . drupal_attributes($attributes_start) . '>' . $date1 . '</span>';
+  $end_date = '<span class="date-display-end"' . drupal_attributes($attributes_end) . '>' . $date2 . $timezone . '</span>';
+
+  // If microdata attributes for the start date property have been passed in,
+  // add the microdata in meta tags.
+  if (!empty($variables['add_microdata'])) {
+    $start_date .= '<meta' . drupal_attributes($variables['microdata']['value']['#attributes']) . '/>';
+    $end_date .= '<meta' . drupal_attributes($variables['microdata']['value2']['#attributes']) . '/>';
+  }
+
+  // Wrap the result with the attributes.
+  return t('!start-date - !end-date', array(
+    '!start-date' => $start_date,
+    '!end-date' => $end_date,
+  ));
+}
 
 ?>
